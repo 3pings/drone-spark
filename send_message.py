f5c3d46da263d9c6b33dba36da4cf16a0dbf1490
3pings <jbarksdale@threepings.com>
2016-11-03 17:26:03 -0400

    added avatar

--- send_message.py
+++ send_message.py
@@ -58,7 +58,7 @@ def verify_roomId(roomId):
     else:
         return False
 
-
+'''
 def get_avatar(avatar):
     '''
     Grab avatar information from Spark API
@@ -74,7 +74,7 @@ def get_avatar(avatar):
         return True
     else:
         return False
-
+'''
 def standard_message(payload):
     '''
     This will create a standard notification message.
@@ -99,7 +99,7 @@ def standard_message(payload):
 def send_message(message_data, message_text):
 
     message_data["markdown"] = message_text
-    message_data["files"] = avatar
+    message_data["files"] = https://1efa7a94ed216783e352-c62266528714497a17239ececf39e9e2.ssl.cf1.rackcdn.com/V1~912aecc551925129248b4a4a8be4f4c6~SWUSmkWORbeicuKhuSPxlQ==~1600
 
     response = requests.post(
         spark_urls["messages"],
